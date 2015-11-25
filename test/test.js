68dda5b55df31995bb1a939d90c5a7d355bdc794
moptym <moptym@163.com>
2015-11-25 15:22:57 +0800

    create test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('The Ultimate Test', function() {
+  describe('The Answer To Life, The Universe, And Everything', function() {
+    it('should be 42', function() {
+      assert.equal(6 * 7, 42);
+    });
+  });
+});
